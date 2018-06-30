7fa9bdb0f493467b7160be439304dccc23305a22
Maarten Vos <maarten@mrtn.run>
2018-06-30 14:09:57 +0200

    fixed example proving reference strings instead of strings

--- examples/main.rs
+++ examples/main.rs
@@ -3,6 +3,6 @@ extern crate bungie;
 use bungie::BungieClient;
 
 fn main() {
-    let bungie = BungieClient::new("<api-key>").with_authentication_token("<oauth-token>");
+    let bungie = BungieClient::new("<api-key>".to_owned()).with_authentication_token("<oauth-token>".to_owned());
     let manifest = bungie.destiny2().get_destiny_manifest();
 }
